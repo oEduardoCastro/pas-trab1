9e4310f3e69b157a6d1e748786ee928574bdeb7e
Eduardo Castro <136605812+oEduardoCastro@users.noreply.github.com>
2025-05-01 01:21:00 +0000

    Criada classe Cliente

---
+++ trabalho1/src/main/java/br/pucrs/eduardocastro002/trabalho1/Cliente.java
@@ -0,0 +1,36 @@
+package br.pucrs.eduardocastro002.trabalho1;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String codigoInterno;
+
+    public Cliente(String nome, String cpf, String telefone, String email, String codigo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.codigoInterno = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getCodigo() {
+        return codigoInterno;
+    }
+}
